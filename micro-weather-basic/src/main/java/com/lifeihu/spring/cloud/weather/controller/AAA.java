426e5946f5a33c90580ad0ed76128766905bce05
e-lifeihu <a12345678>
2018-11-29 14:17:31 +0800

    第一次提交

---
+++ micro-weather-basic/src/main/java/com/lifeihu/spring/cloud/weather/controller/AAA.java
@@ -0,0 +1,13 @@
+package com.lifeihu.spring.cloud.weather.controller;
+
+/**
+ * Created by e-lifeihu on 2018/11/29.
+ */
+public class AAA {
+    public static void main(String[] args) {
+
+        System.out.println("第一次提交");
+
+
+    }
+}
